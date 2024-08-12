80ce422fe6823993969bc9e606ba5e3dd8fb93cc
Abhideep Kumar <itsabhideep@gmail.com>
2024-08-12 17:18:51 +0530

    mysql setup locally

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 //craete an express server
 const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
 const app = express();
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello World');
@@ -8,3 +11,20 @@ app.get('/', (req, res) => {
 app.listen(8080, () => {
     console.log('Server is running on port 8080');
 });
+
+const db=mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'flashcards',
+});
+
+app.get('/cards', (req, res) => {
+    db.query('SELECT * FROM cards', (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    });
+})
